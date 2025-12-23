7b946c342a9decdee771ce3c575b4dc8d19fe527
fsitter <fsitter@42.com>
2025-12-24 00:23:24 +0100

    add base case and permutations, missing safe check

--- ex03/nquenns/n_quenns.c
+++ ex03/nquenns/n_quenns.c
@@ -1,8 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 char	**create_field(int n);
 char	**free_field(char **field, int field_size);
+void	n_queens(char **field, int start, int end);
+int		safe();
+
+int safe()
+{
+	return 1;
+}
+
+void	n_queens(char **field, int start, int end)
+{
+	if (start == end)
+	{
+		int i = 0;
+		int j;
+		int queen;
+		while (i < end-1)
+		{
+			j = 0;
+			queen = 0;
+			while (j < end)
+			{
+				if (field[i][j] != 48)   
+				{
+					queen = j;
+				}
+				j++;
+			}
+			printf("%i ", queen);
+			i++;
+		}
+		j = 0;
+		queen = 0;
+		while (j < end)
+		{
+			if (field[i][j] != 48)
+			{
+				queen = j;
+			}
+			j++;
+		}
+		printf("%i\n", queen);
+	}
+	else
+	{
+		int col = 0;
+		while (col < end)
+		{
+			if (safe())
+			{
+				field[start][col] = 81;
+				n_queens(field, start+1, end);
+				field[start][col] = 48;
+			}
+			col++;
+		}
+	}
+}
+
 
 char	**create_field(int n)
 {
@@ -53,11 +112,19 @@ int	main(int ac, char **av)
 
 	n = 4;
 	field = create_field(n);
-		i = 0;
+
+	// field[0][1] = 'Q';
+	// field[1][3] = 'Q';
+	// field[2][0] = 'Q';
+	// field[3][2] = 'Q';
+
+
+	// print
+	i = 0;
 	while (i < n)
-	{
-		printf("%s\n", field[i]);
-		i++;
-	}
+		printf("%s\n", field[i++]);
+
+	n_queens(field, 0, n);
+	// free
 	field = free_field(field, n);
 }
