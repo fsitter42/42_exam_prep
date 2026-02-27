4fd6e860a0d94b202f08e3c51e1382f8b3630741
fsitter42 <fsitter@42vienna.com>
2026-02-27 11:14:50 +0100

    main.c

--- ex04/level1/picoshell/picoshell.c
+++ ex04/level1/picoshell/picoshell.c
@@ -1,6 +1,16 @@
 #include <unistd.h>
-#include <>
-#include <>
-#include <>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+//need?
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 int    picoshell(char **cmds[]);
+
+int    picoshell(char **cmds[])
+{
+    
+}
