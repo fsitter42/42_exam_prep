5c0df81fb1b11950eb478cd0c0a941b6c4a0deb1
forkroyal <gfabits12@gmail.com>
2026-03-02 15:25:15 +0100

    pico

--- ex04/level1/picoshell/picoshell.c
+++ ex04/level1/picoshell/picoshell.c
@@ -12,5 +12,61 @@ int    picoshell(char **cmds[]);
 
 int    picoshell(char **cmds[])
 {
-    
+    pid_t pid; // für fork
+	int fd[2]; // für pipe
+	int last_fd = -600; // als markierung für start
+	int i = 0; // um durch cmd zu iterieren
+	
+	while (cmds[i])
+	{
+		if (cmds[i + 1]) // wenn es noch einen cmd gibt pipe öffnen
+		{
+			if (pipe(fd) == -1)
+				return (1);
+		}
+		pid = fork();
+		if (pid == -1) // wenn fork failed
+		{
+			if (cmds[i + 1]) // wenn wir eine pipe gemacht haben
+			{
+				close(fd[1]);
+				close(fd[0]);
+			}
+			return (1);
+		}
+		else if (pid == 0) // im kind
+		{
+			if (last_fd != -1) // wenn nicht der erste cmd, read ende auf stdin stecken 0 zu 0
+			{
+				if (dup2(last_fd, 0) == -1)
+					exit (1);
+				close(last_fd);
+			}
+			if (cmds[i + 1]) // wenn nicht der letzt cmd, write ende auf stdout 1 zu 1
+			{
+				close(fd[0]);
+				if (dup2(fd[1], 1) == -1)
+					exit (1);
+				close(fd[1]);
+			}
+			execvp(cmds[i][0], cmds[i]);
+			exit (1);
+		}
+		else // im the parent
+		{
+			if (last_fd != -1) // wenn nicht der erste command den von davor schließen
+				close(last_fd);
+			if (cmds[i + 1]) // wenn es noch einen cmd gibt schreiben schließen und last auf lesen setzten damit kind aus last lesen kann
+			{
+				close(fd[1]);
+				last_fd = fd[0];
+			}
+		}
+		i++;
+	}
+	while (wait(NULL) > 0)
+	{
+		;
+	}
+	return (0);
 }
